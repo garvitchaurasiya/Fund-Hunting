06b89bb3c04f5cfb54d3d318523c43a0c5684126
Garvit Chaurasiya <garvitchaurasiya725@gmail.com>
2022-08-10 14:05:22 +0200

    Connected to the mongoDB Cloud

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 // const mongoDB_URI = "mongodb+srv://igarvit:garvit1146@cluster0.vledw.mongodb.net/northflex?retryWrites=true&w=majority";
-const mongoDB_URI = "mongodb://localhost:27017/fundhunting";
+const mongoDB_URI = "mongodb+srv://garvit:20020725@cluster0.nhpd38n.mongodb.net/?retryWrites=true&w=majority";
+// const mongoDB_URI = "mongodb://localhost:27017/fundhunting";
 
 const connectToMongoDB = ()=>{
     mongoose.connect(mongoDB_URI, ()=>{
