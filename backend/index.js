d631dc98419ffecfbad85918756992e33835578b
Garvit Chaurasiya <garvitchaurasiya725@gmail.com>
2022-08-10 13:28:44 +0200

    Used environment variable with port

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 var cors = require('cors')
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 connectToMongo();
 app.use(cors())
